f29b5c1d819c0b8bfb9e77a3251c3492982305e2
clienthax <clienthax@gmail.com>
2017-06-25 23:13:18 +0100

    Attempt to add ids

--- index.js
+++ index.js
@@ -16,17 +16,22 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 module.exports = function () {
 
+var id = 0;
+
   var write = function write(line, enc, cb) {
 
     if (!line.trim()) return cb();
 
     var vttLine = line.replace(/(WEBVTT\s*(FILE)?.*)(\r\n)*/g, '').replace(/(\d{2}:\d{2}:\d{2})\.(\d{3}\s+)\-\-\>(\s+\d{2}:\d{2}:\d{2})\.(\d{3}\s*)/g, '$1,$2-->$3,$4').replace(/\<.+\>(.+)/g, '$1').replace(/\<.+\>(.+)\<.+\/\>/g, '$1') + '\r\n';
 
+	vttLine = id + '\r\n';
+	
     if (!vttLine.trim()) return cb();
 
     if (/^[0-9]+$/m.test(vttLine)) {
       vttLine = '\r\n' + vttLine;
     }
+	id += 1;
 
     cb(null, vttLine);
   };
